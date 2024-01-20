fea026d72263d602164b1bbc06436d79000197c2
SeongHyunJeon <102949933+SeongHyunJeon@users.noreply.github.com>
2024-01-20 16:33:30 +0900

    Modifier practice

---
+++ ComposePractice/app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -0,0 +1,151 @@
+package com.example.composepractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composepractice.ui.theme.ComposePracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePracticeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    composeTutorial(Modifier.fillMaxSize())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun composeTutorial(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier
+                .weight(0.5f)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = colorResource(R.color.first))
+                    .padding(16.dp)
+                    .weight(0.5f)
+            ) {
+                Text(
+                    text = stringResource(R.string.title1_text),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.body1_text),
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = colorResource(R.color.second))
+                    .padding(16.dp)
+                    .weight(0.5f)
+            ) {
+                Text(
+                    text = stringResource(R.string.title2_text),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.body2_text),
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+        Row(
+            modifier = Modifier.weight(0.5f)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = colorResource(R.color.third))
+                    .padding(16.dp)
+                    .weight(0.5f)
+            ) {
+                Text(
+                    text = stringResource(R.string.title3_text),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.body3_text),
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .background(color = colorResource(R.color.forth))
+                    .weight(0.5f)
+            ) {
+                Text(
+                    text = stringResource(R.string.title4_text),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.body4_text),
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposePracticeTheme {
+        composeTutorial(Modifier.fillMaxSize())
+    }
+}
+
+/*
+수정자 Modifier를 이용한 연산들은 순서에 영향을 받는다. <패딩을 적용하고 백그라운드를 적용하는 것과 그 반대의 결과가 다르다.>
+Text(textAlign)의 Justify는 텍스트를 너비의 여백에 맞추어 정렬한다.
+Modifier.weight()로 해당 요소가 상위 컨테이너에서 차지하는 공간의 가중치를 설정할 수 있다.
+
+ */
